0df115f723699927ab34d8a444dbd0334c9e5a2a
Omar Allouch <allouchomar1@gmail.com>
2024-12-11 21:49:57 +0100

    [docs] fix typo

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def cli():
 def quick_commit():
     """
     Interactive add-commit-push cycle.
-    You can call this command by running `easy-git quick-commit` or `easy-git qc` for short.
+    You can call this command by running `easy-git quick-commit` or `easy-git c` for short.
     This will first list all changed files (tracked and untracked), then prompt you to select the files you want to stage.
     After staging the files, you can choose to either make a new commit or amend the last commit.
     Finally, you can choose to push the changes to the remote.
